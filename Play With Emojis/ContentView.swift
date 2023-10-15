892b3639ddcfd35d671b0f83ef59be10806b3187
Anurag Pathak <anuragpathak911@gmail.com>
2023-10-15 22:12:57 +0530

    Initial Commit

---
+++ Play With Emojis/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Play With Emojis
+//
+//  Created by Anurag Pathak on 15/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Play With Emojis/Play_With_EmojisApp.swift
@@ -0,0 +1,17 @@
+//
+//  Play_With_EmojisApp.swift
+//  Play With Emojis
+//
+//  Created by Anurag Pathak on 15/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Play_With_EmojisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
